83fb8f7230b1ada684221268db8504cd4bb613c5
soccerkevin <soccerkevint11@gmail.com>
2019-06-07 21:50:17 -0700

    basic

---
+++ src/components/Board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/Board.css';
+
+function App() {
+  return (
+    <div className="board">
+      board
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Chess.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './Board';
+import '../stylesheets/Chess.css';
+
+function App() {
+  return (
+    <div className="chess">
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
